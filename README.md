1cd4b54b27e8d1f87f4f622358fc48b942439d96
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2022-01-20 16:44:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -17,3 +17,31 @@ $('#txt1').autocomplete({
   source: list1
 });
 ```
+
+
+
+## setWidthAsTextBox : true | false
+
+New property introduced in some scenario while seeing mobile phone list exceeds the text width for that purpose this field used.
+
+![image-20220120163044455](README.assets/image-20220120163044455.png)
+
+
+
+![image-20220120163029759](README.assets/image-20220120163029759.png)
+
+## Usage
+
+- Load the `jquery.ui.autocomplete.scroll.min.js` script file after loading `jquery.ui.autocomplete.js` etc.
+- The `maxShowItems,setWidthAsTextBox  ` is added to Autocomplete options. It accept the number of items which is max height of items list.
+
+## Example
+
+```js
+$('#txt1').autocomplete({
+  maxShowItems: 5, // Make list height fit to 5 items when items are over 5.
+  setWidthAsTextBox : true
+  source: list1
+});
+```
+
--- jquery.ui.autocomplete.scroll.js
+++ jquery.ui.autocomplete.scroll.js
@@ -6,32 +6,43 @@
  * Licensed under the MIT license.
  */
 
-;(function($, undefined) {
-'use strict';
+; (function ($, undefined) {
+    'use strict';
 
-$.widget('ui.autocomplete', $.ui.autocomplete, {
-  _resizeMenu: function() {
-    var ul, lis, ulW, barW;
-    if (isNaN(this.options.maxShowItems)) { return; }
-    ul = this.menu.element
-      .scrollLeft(0).scrollTop(0) // Reset scroll position
-      .css({overflowX: '', overflowY: '', width: '', maxHeight: ''}); // Restore
-    lis = ul.children('li').css('whiteSpace', 'nowrap');
+    $.widget('ui.autocomplete', $.ui.autocomplete, {
+        _resizeMenu: function () {
+            var ul, lis, ulW, barW;
+            if (isNaN(this.options.maxShowItems)) { return; }
+            ul = this.menu.element
+              .scrollLeft(0).scrollTop(0) // Reset scroll position
+              .css({ overflowX: '', overflowY: '', width: '', maxHeight: '' }); // Restore
+            lis = ul.children('li').css('whiteSpace', 'nowrap');
+            if (this.options.setWidthAsTextBox === true) {
+                lis = ul.children('li').css('whiteSpace', 'normal');
+            }
 
-    if (lis.length > this.options.maxShowItems) {
-      ulW = ul.prop('clientWidth');
-      ul.css({overflowX: 'hidden', overflowY: 'auto',
-        maxHeight: lis.eq(0).outerHeight() * this.options.maxShowItems + 1}); // 1px for Firefox
-      barW = ulW - ul.prop('clientWidth');
-      ul.width('+=' + barW);
-    }
 
-    // Original code from jquery.ui.autocomplete.js _resizeMenu()
-    ul.outerWidth(Math.max(
-      ul.outerWidth() + 1,
-      this.element.outerWidth()
-    ));
-  }
-});
+            if (lis.length > this.options.maxShowItems) {
+                ulW = ul.prop('clientWidth');
+                ul.css({
+                    overflowX: 'hidden', overflowY: 'auto',
+                    maxHeight: lis.eq(0).outerHeight() * this.options.maxShowItems + 1
+                }); // 1px for Firefox
+                barW = ulW - ul.prop('clientWidth');
+                ul.width('+=' + barW);
+            }
+
+            // Original code from jquery.ui.autocomplete.js _resizeMenu()
+            ul.outerWidth(Math.max(
+              ul.outerWidth() + 1,
+              this.element.outerWidth()
+            ));
+
+            if (this.options.setWidthAsTextBox === true) {
+                ul.outerWidth($(this.element).css("width"));
+            }
+
+        }
+    });
 
 })(jQuery);
